60bcbdf2dca60db9691cb0191d4c0a541d6878e6
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-12-08 11:05:24 +0100

    add the dto to get the necessary company info to register for vat

---
+++ Taxually.TechnicalTest/Model/VatRegistrationRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Taxually.TechnicalTest.Model;
+
+public class VatRegistrationRequestDto
+{
+    public string CompanyName { get; set; }
+    public string CompanyId { get; set; }
+    public string Country { get; set; }
+}
