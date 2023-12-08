6e3ac02c7781782c21b5574814abbd6cabd41a69
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-12-08 11:11:29 +0100

    add client interfaces for Http and Queue Taxually clients, and move them to service directory

--- Taxually.TechnicalTest/Service/Client/TaxuallyHttpClient.cs
+++ Taxually.TechnicalTest/Service/Client/TaxuallyHttpClient.cs
@@ -1,6 +1,6 @@
-﻿namespace Taxually.TechnicalTest
+﻿namespace Taxually.TechnicalTest.Service.Client
 {
-    public class TaxuallyHttpClient
+    public class TaxuallyHttpClient : ITaxuallyHttpClient
     {
         public Task PostAsync<TRequest>(string url, TRequest request)
         {
